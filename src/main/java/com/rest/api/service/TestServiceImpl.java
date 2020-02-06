7b435046fad923e8e3f8d903bde1db3b7ed153c7
JM <jeongmin.kim@jinisys.net>
2020-02-06 16:23:21 +0900

    기능추가

---
+++ src/main/java/com/rest/api/service/TestServiceImpl.java
@@ -0,0 +1,19 @@
+package com.rest.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestServiceImpl implements TestService {
+
+    @Override
+    public void test() {
+        System.out.println("===================== 한글 ㄴㅇ러ㅏ닝런아ㅣㄹㄴ어라ㅣㄴㅇ러ㅣㅏ =========================");
+        System.out.println("==========================================================");
+        System.out.println("==========================================================");
+        System.out.println("==========================================================");
+        System.out.println("==========================================================");
+        System.out.println("==========================================================");
+        System.out.println("==========================================================");
+        System.out.println("==========================================================");
+    }
+}
---
+++ src/main/java/com/rest/api/service/security/CustomUserDetailService.java
@@ -0,0 +1,17 @@
+package com.rest.api.service.security;
+
+import com.rest.api.advice.exception.CUserNotFoundException;
+import com.rest.api.repo.UserJpaRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService {
+    private final UserJpaRepo userJpaRepo;
+
+    public UserDetails loadUserByUsername(String userPk) {
+        return userJpaRepo.findById(Long.valueOf(userPk)).orElseThrow(CUserNotFoundException::new);
+    }
+}
